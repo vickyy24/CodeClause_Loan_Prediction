93e02fec00fc4b947a9cb56ef2ce74507c3c218b
Vikas Sontakke <107683378+vickyy24@users.noreply.github.com>
2023-02-09 22:05:42 +0530

    Add files via upload

---
+++ Loan Prediction task 1.py
@@ -0,0 +1,495 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+import warnings
+warnings.filterwarnings("ignore")
+
+
+# In[9]:
+
+
+train = pd.read_csv("C:/Users/cinep/Downloads/archive/train_u6lujuX_CVtuZ9i.csv")
+test = pd.read_csv("C:/Users/cinep/Downloads/archive/test_Y3wMUE5_7gLdaTN.csv")
+
+
+# In[10]:
+
+
+train_original=train.copy()
+test_original=test.copy()
+
+
+# In[11]:
+
+
+train.columns
+
+
+# In[12]:
+
+
+test.columns
+
+
+# In[13]:
+
+
+train.dtypes
+
+
+# In[14]:
+
+
+train.shape
+
+
+# In[15]:
+
+
+test.shape
+
+
+# In[16]:
+
+
+plt.figure(1)
+plt.subplot(121)
+df=train.dropna()
+sns.distplot(df['LoanAmount']);
+
+plt.subplot(122)
+train['LoanAmount'].plot.box(figsize=(16,5))
+
+plt.show()
+
+
+# In[17]:
+
+
+train['Loan_Status'].value_counts()
+
+
+# In[18]:
+
+
+# Normalize can be set to True to print proportions instead of number 
+train['Loan_Status'].value_counts(normalize=True)
+
+
+# In[19]:
+
+
+train['Loan_Status'].value_counts().plot.bar()
+
+
+# In[20]:
+
+
+plt.figure(1)
+plt.subplot(221)
+train['Gender'].value_counts(normalize=True).plot.bar(figsize=(20,10), title= 'Gender')
+
+plt.subplot(222)
+train['Married'].value_counts(normalize=True).plot.bar(title= 'Married')
+
+plt.subplot(223)
+train['Self_Employed'].value_counts(normalize=True).plot.bar(title= 'Self_Employed')
+
+plt.subplot(224)
+train['Credit_History'].value_counts(normalize=True).plot.bar(title= 'Credit_History')
+
+plt.show()
+
+
+# In[21]:
+
+
+plt.figure(1)
+plt.subplot(131)
+train['Dependents'].value_counts(normalize=True).plot.bar(figsize=(24,6), title= 'Dependents')
+
+plt.subplot(132)
+train['Education'].value_counts(normalize=True).plot.bar(title= 'Education')
+
+plt.subplot(133)
+train['Property_Area'].value_counts(normalize=True).plot.bar(title= 'Property_Area')
+
+plt.show()
+
+
+# In[22]:
+
+
+plt.figure(1)
+plt.subplot(121)
+sns.distplot(train['ApplicantIncome']);
+
+plt.subplot(122)
+train['ApplicantIncome'].plot.box(figsize=(16,5))
+
+plt.show()
+
+
+# In[23]:
+
+
+train.boxplot(column='ApplicantIncome', by = 'Education')
+plt.suptitle("")
+
+
+# In[24]:
+
+
+plt.figure(1)
+plt.subplot(121)
+sns.distplot(train['CoapplicantIncome']);
+
+plt.subplot(122)
+train['CoapplicantIncome'].plot.box(figsize=(16,5))
+
+plt.show()
+
+
+# In[25]:
+
+
+Gender=pd.crosstab(train['Gender'],train['Loan_Status'])
+Gender.div(Gender.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True, figsize=(4,4))
+
+
+# In[26]:
+
+
+Married=pd.crosstab(train['Married'],train['Loan_Status'])
+Dependents=pd.crosstab(train['Dependents'],train['Loan_Status'])
+Education=pd.crosstab(train['Education'],train['Loan_Status'])
+Self_Employed=pd.crosstab(train['Self_Employed'],train['Loan_Status'])
+
+Married.div(Married.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True, figsize=(4,4))
+plt.show()
+
+Dependents.div(Dependents.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True)
+plt.show()
+
+Education.div(Education.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True, figsize=(4,4))
+plt.show()
+
+Self_Employed.div(Self_Employed.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True, figsize=(4,4))
+plt.show()
+
+
+# In[27]:
+
+
+Credit_History=pd.crosstab(train['Credit_History'],train['Loan_Status'])
+Property_Area=pd.crosstab(train['Property_Area'],train['Loan_Status'])
+
+Credit_History.div(Credit_History.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True, figsize=(4,4))
+plt.show()
+
+Property_Area.div(Property_Area.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True)
+plt.show()
+
+
+# In[28]:
+
+
+train.groupby('Loan_Status')['ApplicantIncome'].mean().plot.bar()
+
+
+# In[29]:
+
+
+bins=[0,2500,4000,6000,81000]
+group=['Low','Average','High', 'Very high']
+train['Income_bin']=pd.cut(df['ApplicantIncome'],bins,labels=group)
+
+
+# In[30]:
+
+
+Income_bin=pd.crosstab(train['Income_bin'],train['Loan_Status'])
+Income_bin.div(Income_bin.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True)
+plt.xlabel('ApplicantIncome')
+P = plt.ylabel('Percentage')
+
+
+# In[31]:
+
+
+bins=[0,1000,3000,42000]
+group=['Low','Average','High']
+train['Coapplicant_Income_bin']=pd.cut(df['CoapplicantIncome'],bins,labels=group)
+
+
+# In[32]:
+
+
+Coapplicant_Income_bin=pd.crosstab(train['Coapplicant_Income_bin'],train['Loan_Status'])
+Coapplicant_Income_bin.div(Coapplicant_Income_bin.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True)
+plt.xlabel('CoapplicantIncome')
+P = plt.ylabel('Percentage')
+
+
+# In[33]:
+
+
+train['Total_Income']=train['ApplicantIncome']+train['CoapplicantIncome']
+
+
+# In[34]:
+
+
+bins=[0,2500,4000,6000,81000]
+group=['Low','Average','High', 'Very high']
+train['Total_Income_bin']=pd.cut(train['Total_Income'],bins,labels=group)
+
+
+# In[35]:
+
+
+Total_Income_bin=pd.crosstab(train['Total_Income_bin'],train['Loan_Status'])
+Total_Income_bin.div(Total_Income_bin.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True)
+plt.xlabel('Total_Income')
+P = plt.ylabel('Percentage')
+
+
+# In[36]:
+
+
+bins=[0,100,200,700]
+group=['Low','Average','High']
+train['LoanAmount_bin']=pd.cut(df['LoanAmount'],bins,labels=group)
+
+
+# In[37]:
+
+
+LoanAmount_bin=pd.crosstab(train['LoanAmount_bin'],train['Loan_Status'])
+LoanAmount_bin.div(LoanAmount_bin.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True)
+plt.xlabel('LoanAmount')
+P = plt.ylabel('Percentage')
+
+
+# In[38]:
+
+
+train=train.drop(['Income_bin', 'Coapplicant_Income_bin', 'LoanAmount_bin', 'Total_Income_bin', 'Total_Income'], axis=1)
+
+
+# In[39]:
+
+
+train['Dependents'].replace('3+', 3,inplace=True)
+test['Dependents'].replace('3+', 3,inplace=True)
+train['Loan_Status'].replace('N', 0,inplace=True)
+train['Loan_Status'].replace('Y', 1,inplace=True)
+
+
+# In[40]:
+
+
+matrix = train.corr()
+f, ax = plt.subplots(figsize=(9, 6))
+sns.heatmap(matrix, vmax=.8, square=True, cmap="BuPu");
+
+
+# In[41]:
+
+
+train.isnull().sum()
+
+
+# In[42]:
+
+
+train['Gender'].fillna(train['Gender'].mode()[0], inplace=True)
+train['Married'].fillna(train['Married'].mode()[0], inplace=True)
+train['Dependents'].fillna(train['Dependents'].mode()[0], inplace=True)
+train['Self_Employed'].fillna(train['Self_Employed'].mode()[0], inplace=True)
+train['Credit_History'].fillna(train['Credit_History'].mode()[0], inplace=True)
+
+
+# In[43]:
+
+
+train['Loan_Amount_Term'].value_counts()
+
+
+# In[44]:
+
+
+train['Loan_Amount_Term'].fillna(train['Loan_Amount_Term'].mode()[0], inplace=True)
+
+
+# In[45]:
+
+
+train['LoanAmount'].fillna(train['LoanAmount'].median(), inplace=True)
+
+
+# In[46]:
+
+
+train.isnull().sum()
+
+
+# In[47]:
+
+
+test['Gender'].fillna(train['Gender'].mode()[0], inplace=True)
+test['Dependents'].fillna(train['Dependents'].mode()[0], inplace=True)
+test['Self_Employed'].fillna(train['Self_Employed'].mode()[0], inplace=True)
+test['Credit_History'].fillna(train['Credit_History'].mode()[0], inplace=True)
+test['Loan_Amount_Term'].fillna(train['Loan_Amount_Term'].mode()[0], inplace=True)
+test['LoanAmount'].fillna(train['LoanAmount'].median(), inplace=True)
+
+
+# In[48]:
+
+
+train['LoanAmount_log'] = np.log(train['LoanAmount'])
+train['LoanAmount_log'].hist(bins=20)
+test['LoanAmount_log'] = np.log(test['LoanAmount'])
+
+
+# In[49]:
+
+
+train=train.drop('Loan_ID',axis=1)
+test=test.drop('Loan_ID',axis=1)
+
+
+# In[50]:
+
+
+X = train.drop('Loan_Status',1)
+y = train.Loan_Status
+
+
+# In[51]:
+
+
+X=pd.get_dummies(X)
+train=pd.get_dummies(train)
+test=pd.get_dummies(test)
+
+
+# In[52]:
+
+
+from sklearn.model_selection import train_test_split
+
+
+# In[53]:
+
+
+x_train, x_cv, y_train, y_cv = train_test_split(X,y, test_size =0.3)
+
+
+# In[54]:
+
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+
+# In[55]:
+
+
+model=LogisticRegression()
+model.fit(x_train, y_train)
+
+
+# In[56]:
+
+
+pred_cv = model.predict(x_cv)
+
+
+# In[57]:
+
+
+accuracy_score(y_cv,pred_cv)
+
+
+# In[58]:
+
+
+pred_test = model.predict(test)
+
+
+# In[59]:
+
+
+from sklearn.model_selection import StratifiedKFold
+
+
+# In[60]:
+
+
+i=1
+kf = StratifiedKFold(n_splits=5,random_state=1,shuffle=True)
+for train_index,test_index in kf.split(X,y):
+     print('\n{} of kfold {}'.format(i,kf.n_splits))
+     xtr,xvl = X.loc[train_index],X.loc[test_index]
+     ytr,yvl = y[train_index],y[test_index]
+    
+     model = LogisticRegression(random_state=1)
+     model.fit(xtr, ytr)
+     pred_test = model.predict(xvl)
+     score = accuracy_score(yvl,pred_test)
+     print('accuracy_score',score)
+     i+=1
+pred_test = model.predict(test)
+pred=model.predict_proba(xvl)[:,1]
+
+
+# In[61]:
+
+
+from sklearn import metrics
+fpr, tpr, _ = metrics.roc_curve(yvl,  pred)
+auc = metrics.roc_auc_score(yvl, pred)
+plt.figure(figsize=(12,8))
+plt.plot(fpr,tpr,label="validation, auc="+str(auc))
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.legend(loc=4)
+plt.show()
+
+
+# In[66]:
+
+
+test['Loan_Status']=pred_test            # filling Loan_Status with predictions 
+test['Loan_ID']=test_original['Loan_ID'] # filling Loan_ID with test Loan_ID
+
+
+# In[68]:
+
+
+test['Loan_Status'].replace(0, 'N',inplace=True) 
+test['Loan_Status'].replace(1, 'Y',inplace=True)
+
+
+# In[77]:
+
+
+# Converting submission file to .csv format 
+pd.DataFrame(test, columns=['Loan_ID','Loan_Status']).to_csv('XGBoost.csv')
+
+
+# In[ ]:
+
+
+#we got  accuracy 0.73611 with this model
+
